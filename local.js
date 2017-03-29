08cc348f0d0ea96e40dfed2d296eecd83ab06a76
LearningNerd <learningnerd@gmail.com>
2017-03-28 17:30:02 -0700

    add boilerplate for socket.io, create public directory for serving files, rename index.js to server.js to avoid confusion

---
+++ local.js
@@ -0,0 +1,5 @@
+// Start a WebSocket connection with the server using SocketIO
+var socket = io();
+	// Note that the SocketIO client-side library and this file (local.js)
+	// were both imported in index.html right before the </body> tag
+
