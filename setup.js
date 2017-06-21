eca5a735b0aeba5e30045734ba876353ab49d7c8
LearningNerd <learningnerd@gmail.com>
2017-06-21 13:47:39 -0700

    Fix setup script to NOT overwrite existing .env file

--- setup.js
+++ setup.js
@@ -1,5 +1,20 @@
 'use strict';
+
 var fs = require('fs');
-fs.createReadStream('.sample-env')
-  .pipe(fs.createWriteStream('.env'));
-console.log('\n\tCreated .env file in project\'s root directory.\n\tRemember to open that file and put your GitHub API secret in there!\n\t(Only for development environments.)\n');
+
+// Try to create .env file but don't overwrite if it already exists
+fs.open('.env', "wx", (err, fd) => {
+	if (err) {
+		// For any error other than "file already exists", throw it!
+		if (err.code !== "EEXIST") {
+			throw err;
+		}
+		// Stop the function here if there were ANY errors
+		return;
+	}
+
+	// If creating/opening the .env file worked, write to it from .sample-env
+	fs.createReadStream('.sample-env').pipe(fs.createWriteStream("", {fd: fd}));
+
+	console.log('\n\tCreated .env file in project\'s root directory.\n\tRemember to open that file and put your GitHub API secret in there!\n\t(Only for development environments.)\n');
+});
